d8b91c2433f254fa6e43bb24ba87a14d43fd7441
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-05 11:34:42 +0300

    reduce exercise

---
+++ reduce_exrcise.js
@@ -0,0 +1,210 @@
+const webpage = `
+Jump to navigationJump to search
+Welcome to Wikipedia,
+the free encyclopedia that anyone can edit.
+6,135,168 articles in English
+ArtsBiographyGeographyHistoryMathematicsScienceSocietyTechnologyAll portals
+From today's featured article
+Eurasian crag martin
+The Eurasian crag martin (Ptyonoprogne rupestris) is a small swallow with brown upperparts, paler underparts, and a white-spotted square tail. It breeds in mountains in southern Eurasia and northwestern Africa. It is larger and has brighter tail spots than the three other species in its genus. Many European birds are resident, but northern and Asian populations winter in north Africa, the Middle East or India. This martin builds a half-cup mud nest lined with soft material under a cliff overhang or on a building, and the female lays two to five brown-blotched white eggs, incubated mainly by her although both parents feed the chicks. The martin feeds on insects that are caught as it flies near cliff faces or over open country. Adults and young may be hunted by birds of prey or corvids, and may host blood-sucking mites. With its large and expanding range and population there are no significant conservation concerns. (This article is part of a featured topic: Crag martins.)
+
+Recently featured: James Thompson (surveyor)Banksia sessilisTrials of Mana
+ArchiveBy emailMore featured articles
+Did you know ...
+Palika Kendra
+Palika Kendra
+... that the Palika Kendra (pictured) is one of 23 buildings in India documented in the Atlas of Brutalist Architecture?
+... that in order to serve as a prosecutor at the Nuremberg trials, Cecelia Goetz had to receive a "waiver of disability" from Telford Taylor because she was a woman?
+... that to reduce greenhouse gas emissions from shipping, the e5 Project is producing solar- and wind-powered ships to transport fuel oil?
+... that ornithologist Corina Newsome and herpetologist Earyn McGee held events as part of the inaugural Black Birders Week, an effort to highlight the challenges faced by Black nature enthusiasts?
+... that the presence of the umbrella slug in the Bay of Biscay, first reported in 2016, may be an example of "tropicalisation"?
+... that Luton Town F.C. chairman John Gurney organised a "Manager Idol" phone-vote in order to find the club's new manager?
+... that choreographer Kenneth MacMillan turned a duet in his ballet Concerto into a female solo when the male dancer broke his foot prior to the premiere?
+... that retired United States Air Force colonel Kim Olson is a Master Gardener?
+ArchiveStart a new articleNominate an article
+In the news
+COVID-19 pandemic
+DiseaseVirusTestingTimelineBy locationImpactNotable deathsPortal
+Damage from the Port of Beirut explosions
+Damage from the Port of Beirut explosions
+Multiple explosions (aftermath pictured) in Beirut, Lebanon, kill at least seventy people and injure more than four thousand others.
+Former King of Spain Juan Carlos I goes into voluntary exile following a financial scandal.
+After five months of court challenges, the People's Progressive Party, led by Irfaan Ali, is declared the winner of the Guyanese general election.
+Former Malaysian prime minister Najib Razak is found guilty on all seven charges in the first trial related to the 1MDB scandal.
+Recent deaths: Ralph BarbieriJohn HumeLeon FleisherKamal Rani VarunWilford BrimleyBill English
+Other recent eventsNominate an article
+On this day
+August 5: Victory and Homeland Thanksgiving Day in Croatia (1995); Tu B'Av (Judaism, 2020)
+
+Marilyn Monroe
+Marilyn Monroe
+1816 – Sir John Barrow, secretary at the Admiralty, rejected a proposal to use Francis Ronalds's electrical telegraph, deeming it "wholly unnecessary".
+1916 – First World War: The British Empire's Sinai and Palestine campaign began with a victory at the Battle of Romani.
+1962 – American actress and model Marilyn Monroe (pictured) was found dead of a barbiturate overdose in her home in Brentwood, Los Angeles, an event that has become the center of one of the most debated conspiracy theories.
+1984 – A Biman Bangladesh Airlines aircraft crashed while attempting to land in Dhaka, killing 49 people in the deadliest aviation accident in Bangladeshi history.
+Edmund of Woodstock (b. 1301)Harry Trott (b. 1866)Alec Guinness (d. 2000)
+More anniversaries: August 4August 5August 6
+ArchiveBy emailList of historical anniversaries
+Today's featured picture
+Augusta Victoria Hospital
+The Augusta Victoria Hospital is a church-hospital complex located on the northern side of the Mount of Olives in East Jerusalem, one of six hospitals in the East Jerusalem Hospitals Network. The hospital provides specialty care for Palestinians from across the West Bank and the Gaza Strip, with services including a cancer center, a dialysis unit, and a pediatric center. It is the second largest hospital in East Jerusalem, as well as the sole remaining specialized care unit located in the West Bank or Gaza Strip. The complex also includes the German Protestant Church of the Ascension, with a circa 50-metre-tall (160 ft) bell tower.
+
+Photograph credit: Ori Aloni
+
+Recently featured: Strip photographyReineSpotted trunkfish
+ArchiveMore featured pictures
+Other areas of Wikipedia
+Community portal – Bulletin board, projects, resources and activities covering a wide range of Wikipedia areas.
+Help desk – Ask questions about using Wikipedia.
+Local embassy – For Wikipedia-related communication in languages other than English.
+Reference desk – Serving as virtual librarians, Wikipedia volunteers tackle your questions on a wide range of subjects.
+Site news – Announcements, updates, articles and press releases on Wikipedia and the Wikimedia Foundation.
+Village pump – For discussions about Wikipedia itself, including areas for technical issues and policies.
+Wikipedia's sister projects
+Wikipedia is hosted by the Wikimedia Foundation, a non-profit organization that also hosts a range of other projects:
+
+Commons Commons
+Free media repository
+MediaWiki MediaWiki
+Wiki software development
+Meta-Wiki Meta-Wiki
+Wikimedia project coordination
+Wikibooks Wikibooks
+Free textbooks and manuals
+Wikidata Wikidata
+Free knowledge base
+Wikinews Wikinews
+Free-content news
+Wikiquote Wikiquote
+Collection of quotations
+Wikisource Wikisource
+Free-content library
+Wikispecies Wikispecies
+Directory of species
+Wikiversity Wikiversity
+Free learning resources
+Wikivoyage Wikivoyage
+Free travel guide
+Wiktionary Wiktionary
+Dictionary and thesaurus
+Wikipedia languages
+This Wikipedia is written in English. Started in 2001, it currently contains 6,135,168 articles. Many other Wikipedias are available; some of the largest are listed below.
+
+More than 1,000,000 articles: العربيةDeutschEspañolFrançaisItalianoNederlands日本語PolskiPortuguêsРусскийSvenskaУкраїнськаTiếng Việt中文
+More than 250,000 articles: Bahasa IndonesiaBahasa MelayuBân-lâm-gúБългарскиCatalàČeštinaDanskEsperantoEuskaraفارسی‎עברית한국어MagyarNorsk BokmålRomânăSrpskiSrpskohrvatskiSuomiTürkçe
+More than 50,000 articles: AsturianuBosanskiEestiΕλληνικάEnglish (Simple English)GalegoHrvatskiLatviešuLietuviųമലയാളംNorsk nynorskSlovenčinaSlovenščinaไทย
+Complete list of Wikipedias
+Navigation menu
+Not logged inTalkContributionsCreate accountLog in
+Main PageTalk
+ReadView sourceView historySearch
+Search Wikipedia
+Main page
+Contents
+Current events
+Random article
+About Wikipedia
+Contact us
+Donate
+Contribute
+Help
+Community portal
+Recent changes
+Upload file
+Tools
+What links here
+Related changes
+Special pages
+Permanent link
+Page information
+Wikidata item
+Print/export
+Download as PDF
+Printable version
+In other projects
+Wikimedia Commons
+MediaWiki
+Meta-Wiki
+Wikispecies
+Wikibooks
+Wikidata
+Wikimania
+Wikinews
+Wikiquote
+Wikisource
+Wikiversity
+Wikivoyage
+Wiktionary
+
+Languages
+العربية
+Български
+Bosanski
+Català
+Čeština
+Dansk
+Deutsch
+Eesti
+Ελληνικά
+Español
+Esperanto
+Euskara
+فارسی
+Français
+Galego
+한국어
+Hrvatski
+Bahasa Indonesia
+Italiano
+עברית
+ქართული
+Latviešu
+Lietuvių
+Magyar
+Bahasa Melayu
+Nederlands
+日本語
+Norsk bokmål
+Norsk nynorsk
+Polski
+Português
+Română
+Русский
+Simple English
+Slovenčina
+Slovenščina
+Српски / srpski
+Srpskohrvatski / српскохрватски
+Suomi
+Svenska
+ไทย
+Türkçe
+Українська
+Tiếng Việt
+中文
+Complete list
+Text is available under the Creative Commons Attribution-ShareAlike License; additional terms may apply. By using this site, you agree to the Terms of Use and Privacy Policy. Wikipedia® is a registered trademark of the Wikimedia Foundation, Inc., a non-profit organization.
+Privacy policyAbout WikipediaDisclaimersContact WikipediaDevelopersStatisticsCookie statementMobile viewWikimedia FoundationPowered by MediaWiki
+`
+
+
+const isValidChar = (char) => char.match(/[a-z0-9]/i);
+
+const lowercase = char => char.toLowerCase(); 
+const instanceCounter = (counts, char) => {
+  counts[char] ? counts[char] = counts[char] + 1 : counts[char] = 1;
+  return counts;
+}
+
+const result = webpage
+  .split('')
+  .filter(isValidChar)
+  .map(lowercase)
+  .reduce(instanceCounter, {});
+
+const sortByValue = (a, b) => {
+  return a[1] - b[1]
+}
+
+const sortedResult = Object.entries(result).sort(sortByValue);
+console.log(sortedResult);
