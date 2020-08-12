2927a15ef800f7e0a8a0d3891242cddc7c667f93
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-12 11:10:56 +0300

    added closure exercise

--- closure.js
+++
@@ -1,9 +0,0 @@
-function createGame(gamemName) {
-  let score = 0;
-  return function win () {
-    score++;
-    return `Your name ${gameName} score is ${score}`
-  }
-}
-
-const hockey = createGame('Hockey');
